d92db2dcfcd6e4fd30b5b87eb62e0248e73804c1
PROF ROMULO <86451963+romulopena@users.noreply.github.com>
2022-05-17 16:22:44 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+//função criar a dimensão da tela
+function setup() {
+  createCanvas(500, 350);
+}
+//função desenhar tela de fundo
+function draw() {
+  background(0,0,250);
+  
+  //primeiro retangulo
+  fill(250,250,0);
+  stroke(250,250,0);
+  rect(0,175,500,350);
+  
+  //segundo retangulo
+  fill(250,250,0);
+  stroke(250,250,0);
+  rect(0,175,500,350);
+}
